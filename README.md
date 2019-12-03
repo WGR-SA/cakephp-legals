558b2a65ea3bce437f9c595adbd4cf4c9c9b761c
awallef <antoine.wallef@gmail.com>
2019-12-03 15:01:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Legals plugin for CakePHP
+
+## Installation
+
+You can install this plugin into your CakePHP application using [composer](http://getcomposer.org).
+
+The recommended way to install composer packages is:
+
+```bash
+composer require wgr/cakephp-legals
+```
---
+++ src/Plugin.php
@@ -0,0 +1,18 @@
+<?php
+namespace WGR\Legals;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+
+class Plugin extends BasePlugin
+{
+  public function bootstrap(PluginApplicationInterface $app)
+  {
+    parent::bootstrap($app);
+  }
+
+  public function routes($routes)
+  {
+    parent::routes($routes);
+  }
+}
